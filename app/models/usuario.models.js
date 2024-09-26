afcfde2806df52680ff951a0765afe7093f27a53
Thristan14 <thristanmenle@gmail.com>
2024-09-25 18:54:24 -0600

    actualizar

--- app/models/usuario.models.js
+++ app/models/usuario.models.js
@@ -1,28 +1,27 @@
-module.exports = (sequelize, Sequelize) => {
-    const Usuario = sequelize.define('usuarios', {
-        id_usuario: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        nombre: {
-            type: Sequelize.STRING(100),
-            allowNull: false
-        },
-        correo: {
-            type: Sequelize.STRING(100),
-            allowNull: false,
-            unique: true
-        },
-        contrasena: {
-            type: Sequelize.STRING(255),
-            allowNull: false
-        },
-        fecha_creacion: {
-            type: Sequelize.DATE,
-            defaultValue: Sequelize.NOW
-        }
-    });
+const Usuario = sequelize.define('usuarios', {
+    id_usuario: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+    },
+    correo: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+    },
+    contrasena: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+    },
+    fecha_creacion: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    }
+}, {
+    timestamps: false // Desactivar timestamps
+});
 
-    return Usuario;
-};
